2101c834966d1db913ecfccfb6b9b7c8d6db5e16
Roberto Livi <robertomlivi@outlook.com>
2020-03-16 00:49:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   key = nil
   min_value = 0
   name_hash.each do |k, v|
-    if min_value == 0 || min_value < v
+    if min_value == 0 || min_value > v
       min_value = v
       key = k
     end
